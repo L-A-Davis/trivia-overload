18e3a08de592e4d5a1487fc8b25e083285e37b2f
ddbrennan <ddbren@gmail.com>
2018-01-22 11:04:11 -0500

    minor frontend additions

---
+++ front-end/src/app.js
@@ -0,0 +1,12 @@
+class App {
+  static init() {
+    console.log("Hey There")
+
+    //add event listeners
+    fetch("http://localhost:3000/api/v1/questions")
+      .then(response => response.json())
+      .then(console.log)
+  }
+
+  //static handle listeners
+}
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,1 @@
-class App {
-  static init() {
-    console.log("Hey There")
-
-    //add event listeners
-    fetch("http://localhost:3000/api/v1/questions")
-      .then(response => response.json())
-      .then(console.log)
-  }
-
-  //static handle listeners
-}
-
 document.addEventListener("DOMContentLoaded", App.init)
