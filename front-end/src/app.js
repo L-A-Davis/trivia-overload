54c7abcf2c29aba7d33e36dfcdefba2843739a4a
L-A-Davis <lindsay.amanda.davis@gmail.com>
2018-01-22 14:59:39 -0500

    complete category API call

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -5,8 +5,29 @@ class App {
     let startButton = document.getElementById("start-game")
     startButton.addEventListener('click', App.displayQuestions)
 
-    Adapter.getQuestions().then(res => App.getAllQuestions(res.results))
+    App.selectCategory = document.getElementById('select-category')
+    App.selectCategory.addEventListener('change', App.handleCategorySelection)
+
+    Adapter.getCategories().then(res => App.getAllCategories(res.trivia_categories))
+
+
   }
+
+static getAllCategories(resp) {
+  for (let el of resp) {
+    let newCat = new Category(el)
+    let option = document.createElement("option")
+    option.text = newCat.name
+    option.value = newCat.id
+    App.selectCategory.appendChild(option)
+  }
+}
+
+static handleCategorySelection(event) {
+  let category = parseInt(event.target.value)
+  Adapter.getQuestions(category).then(res => App.getAllQuestions(res.results))
+}
+
 
   static getAllQuestions(resp) {
      for (let value of resp){
